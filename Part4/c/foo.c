8d1c007498476ef47006951f1930217ef4b6d7e4
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-01-20 12:06:16 +0100

    Add answers

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -1,21 +1,31 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define ITERATIONS 1000000
+
 int i = 0;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for(int it= 0; it < ITERATIONS; ++it) {
+        ++i;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for(int it= 0; it < ITERATIONS; ++it) {
+        --i;
+    }
     return NULL;
 }
 
 
 int main(){
+    
+    pthread_t incrementingThread, decrementingThread;
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -11,21 +11,26 @@ i = 0
 
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+    for it in range(1000000):
+        i += 1
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
+    for it in range(1000000):
+        i -= 1
 
 
 
 def main():
     # TODO: Something is missing here (needed to print i)
+    print("The starting number is %d" % (i))
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
     
     # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
